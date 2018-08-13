197b2fa79bc3a52e48db73f3a9f05850d8b8576d
Stiven Deleur <sdeleur@bloomberg.net>
2018-08-13 17:35:28 -0400

    added derivative()

--- expr/funcs.go
+++ expr/funcs.go
@@ -56,6 +56,7 @@ func init() {
 		"averageSeries":         {NewAggregateConstructor("average", crossSeriesAvg), true},
 		"consolidateBy":         {NewConsolidateBy, true},
 		"countSeries":           {NewCountSeries, true},
+		"derivative":            {NewDerivative, true},
 		"diffSeries":            {NewAggregateConstructor("diff", crossSeriesDiff), true},
 		"divideSeries":          {NewDivideSeries, true},
 		"divideSeriesLists":     {NewDivideSeriesLists, true},
