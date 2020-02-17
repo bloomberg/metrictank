65f4b935adb3f3a526dc9ccc595df548294ae49f
Sean Hanson <shanson7@bloomberg.net>
2020-02-17 12:48:33 +0000

    Fix logging

--- api/routes.go
+++ api/routes.go
@@ -18,6 +18,7 @@ func (s *Server) RegisterRoutes() {
 	r.Use(middleware.Tracer(s.Tracer))
 	r.Use(macaron.Renderer())
 	r.Use(middleware.OrgMiddleware(multiTenant))
+	r.Use(middleware.Logger())
 	r.Use(middleware.CorsHandler())
 	form := binding.Form
 	bind := binding.Bind
