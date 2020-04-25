a20064e3a48ba16c49afd8e688f5a7b63dd3da7c
Sean Hanson <shanson7@bloomberg.net>
2020-04-25 18:18:26 +0100

    Round 4 of test normalization

--- expr/func_removeemptyseries_test.go
+++ expr/func_removeemptyseries_test.go
@@ -11,50 +11,15 @@ func TestRemoveEmptySeriesIfAtLeastOneNonNull(t *testing.T) {
 	testRemoveEmptySeries(
 		0.0, // xFilesFactor
 		[]models.Series{
-			{
-				Interval:   10,
-				QueryPatt:  "some nulls",
-				Target:     "a",
-				Datapoints: getCopy(a),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "all nulls",
-				Target:     "b",
-				Datapoints: getCopy(allNulls),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "no nulls",
-				Target:     "c",
-				Datapoints: getCopy(c),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "allZeros",
-				Target:     "d",
-				Datapoints: getCopy(allZeros),
-			},
+			getSeries("a", "some nulls", a),
+			getSeries("b", "all nulls", allNulls),
+			getSeries("c", "no nulls", c),
+			getSeries("d", "allZeros", allZeros),
 		},
 		[]models.Series{
-			{
-				Interval:   10,
-				Target:     "removeEmptySeries(a, 0)",
-				QueryPatt:  "removeEmptySeries(a, 0)",
-				Datapoints: getCopy(a),
-			},
-			{
-				Interval:   10,
-				Target:     "removeEmptySeries(c, 0)",
-				QueryPatt:  "removeEmptySeries(c, 0)",
-				Datapoints: getCopy(c),
-			},
-			{
-				Interval:   10,
-				Target:     "removeEmptySeries(d, 0)",
-				QueryPatt:  "removeEmptySeries(d, 0)",
-				Datapoints: getCopy(allZeros),
-			},
+			getSeries("removeEmptySeries(a, 0)", "removeEmptySeries(a, 0)", a),
+			getSeries("removeEmptySeries(c, 0)", "removeEmptySeries(c, 0)", c),
+			getSeries("removeEmptySeries(d, 0)", "removeEmptySeries(d, 0)", allZeros),
 		},
 		t,
 	)
@@ -64,44 +29,14 @@ func TestRemoveEmptySeriesAllowNoNulls(t *testing.T) {
 	testRemoveEmptySeries(
 		1.0, // xFilesFactor
 		[]models.Series{
-			{
-				Interval:   10,
-				QueryPatt:  "some nulls",
-				Target:     "a",
-				Datapoints: getCopy(a),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "all nulls",
-				Target:     "b",
-				Datapoints: getCopy(allNulls),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "no nulls",
-				Target:     "c",
-				Datapoints: getCopy(c),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "allZeros",
-				Target:     "d",
-				Datapoints: getCopy(allZeros),
-			},
+			getSeries("a", "some nulls", a),
+			getSeries("b", "all nulls", allNulls),
+			getSeries("c", "no nulls", c),
+			getSeries("d", "allZeros", allZeros),
 		},
 		[]models.Series{
-			{
-				Interval:   10,
-				Target:     "removeEmptySeries(c, 1)",
-				QueryPatt:  "removeEmptySeries(c, 1)",
-				Datapoints: getCopy(c),
-			},
-			{
-				Interval:   10,
-				Target:     "removeEmptySeries(d, 1)",
-				QueryPatt:  "removeEmptySeries(d, 1)",
-				Datapoints: getCopy(allZeros),
-			},
+			getSeries("removeEmptySeries(c, 1)", "removeEmptySeries(c, 1)", c),
+			getSeries("removeEmptySeries(d, 1)", "removeEmptySeries(d, 1)", allZeros),
 		},
 		t,
 	)
@@ -111,50 +46,15 @@ func TestRemoveEmptySeriesAllow30PercentNulls(t *testing.T) {
 	testRemoveEmptySeries(
 		0.3, // xFilesFactor
 		[]models.Series{
-			{
-				Interval:   10,
-				QueryPatt:  "30 % nulls",
-				Target:     "a",
-				Datapoints: getCopy(a),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "all nulls",
-				Target:     "b",
-				Datapoints: getCopy(allNulls),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "no nulls",
-				Target:     "c",
-				Datapoints: getCopy(c),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "allZeros",
-				Target:     "d",
-				Datapoints: getCopy(allZeros),
-			},
+			getSeries("a", "30 % nulls", a),
+			getSeries("b", "all nulls", allNulls),
+			getSeries("c", "no nulls", c),
+			getSeries("d", "allZeros", allZeros),
 		},
 		[]models.Series{
-			{
-				Interval:   10,
-				Target:     "removeEmptySeries(a, 0.3)",
-				QueryPatt:  "removeEmptySeries(a, 0.3)",
-				Datapoints: getCopy(a),
-			},
-			{
-				Interval:   10,
-				Target:     "removeEmptySeries(c, 0.3)",
-				QueryPatt:  "removeEmptySeries(c, 0.3)",
-				Datapoints: getCopy(c),
-			},
-			{
-				Interval:   10,
-				Target:     "removeEmptySeries(d, 0.3)",
-				QueryPatt:  "removeEmptySeries(d, 0.3)",
-				Datapoints: getCopy(allZeros),
-			},
+			getSeries("removeEmptySeries(a, 0.3)", "removeEmptySeries(a, 0.3)", a),
+			getSeries("removeEmptySeries(c, 0.3)", "removeEmptySeries(c, 0.3)", c),
+			getSeries("removeEmptySeries(d, 0.3)", "removeEmptySeries(d, 0.3)", allZeros),
 		},
 		t,
 	)
@@ -164,44 +64,14 @@ func TestRemoveEmptySeriesAllow70PercentNulls(t *testing.T) {
 	testRemoveEmptySeries(
 		0.7, // xFilesFactor
 		[]models.Series{
-			{
-				Interval:   10,
-				QueryPatt:  "30 % nulls",
-				Target:     "a",
-				Datapoints: getCopy(a),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "all nulls",
-				Target:     "b",
-				Datapoints: getCopy(allNulls),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "no nulls",
-				Target:     "c",
-				Datapoints: getCopy(c),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "allZeros",
-				Target:     "d",
-				Datapoints: getCopy(allZeros),
-			},
+			getSeries("a", "30 % nulls", a),
+			getSeries("b", "all nulls", allNulls),
+			getSeries("c", "no nulls", c),
+			getSeries("d", "allZeros", allZeros),
 		},
 		[]models.Series{
-			{
-				Interval:   10,
-				Target:     "removeEmptySeries(c, 0.7)",
-				QueryPatt:  "removeEmptySeries(c, 0.7)",
-				Datapoints: getCopy(c),
-			},
-			{
-				Interval:   10,
-				Target:     "removeEmptySeries(d, 0.7)",
-				QueryPatt:  "removeEmptySeries(d, 0.7)",
-				Datapoints: getCopy(allZeros),
-			},
+			getSeries("removeEmptySeries(c, 0.7)", "removeEmptySeries(c, 0.7)", c),
+			getSeries("removeEmptySeries(d, 0.7)", "removeEmptySeries(d, 0.7)", allZeros),
 		},
 		t,
 	)
@@ -211,50 +81,15 @@ func TestRemoveEmptySeriesMissingInputXFilesFactor(t *testing.T) {
 	testRemoveEmptySeries(
 		math.NaN(), // xFilesFactor
 		[]models.Series{
-			{
-				Interval:   10,
-				QueryPatt:  "some nulls",
-				Target:     "a",
-				Datapoints: getCopy(a),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "all nulls",
-				Target:     "b",
-				Datapoints: getCopy(allNulls),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "no nulls",
-				Target:     "c",
-				Datapoints: getCopy(c),
-			},
-			{
-				Interval:   10,
-				QueryPatt:  "allZeros",
-				Target:     "d",
-				Datapoints: getCopy(allZeros),
-			},
+			getSeries("a", "some nulls", a),
+			getSeries("b", "all nulls", allNulls),
+			getSeries("c", "no nulls", c),
+			getSeries("d", "allZeros", allZeros),
 		},
 		[]models.Series{
-			{
-				Interval:   10,
-				Target:     "removeEmptySeries(a, 0)",
-				QueryPatt:  "removeEmptySeries(a, 0)",
-				Datapoints: getCopy(a),
-			},
-			{
-				Interval:   10,
-				Target:     "removeEmptySeries(c, 0)",
-				QueryPatt:  "removeEmptySeries(c, 0)",
-				Datapoints: getCopy(c),
-			},
-			{
-				Interval:   10,
-				Target:     "removeEmptySeries(d, 0)",
-				QueryPatt:  "removeEmptySeries(d, 0)",
-				Datapoints: getCopy(allZeros),
-			},
+			getSeries("removeEmptySeries(a, 0)", "removeEmptySeries(a, 0)", a),
+			getSeries("removeEmptySeries(c, 0)", "removeEmptySeries(c, 0)", c),
+			getSeries("removeEmptySeries(d, 0)", "removeEmptySeries(d, 0)", allZeros),
 		},
 		t,
 	)
@@ -265,8 +100,26 @@ func testRemoveEmptySeries(xff float64, in []models.Series, out []models.Series,
 	f.(*FuncRemoveEmptySeries).in = NewMock(in)
 	f.(*FuncRemoveEmptySeries).xFilesFactor = xff
 
-	got, err := f.Exec(make(map[Req][]models.Series))
+	// Copy input to check that it is unchanged later
+	inputCopy := make([]models.Series, len(in))
+	copy(inputCopy, in)
+
+	dataMap := DataMap(make(map[Req][]models.Series))
+
+	got, err := f.Exec(dataMap)
 	if err := equalOutput(out, got, nil, err); err != nil {
 		t.Fatal(err)
 	}
+
+	t.Run("DidNotModifyInput", func(t *testing.T) {
+		if err := equalOutput(inputCopy, in, nil, nil); err != nil {
+			t.Fatalf("Input was modified, err = %s", err)
+		}
+	})
+
+	t.Run("DoesNotDoubleReturnPoints", func(t *testing.T) {
+		if err := dataMap.CheckForOverlappingPoints(); err != nil {
+			t.Fatalf("Point slices in datamap overlap, err = %s", err)
+		}
+	})
 }
