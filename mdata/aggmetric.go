0be3f79dc2d4444ae60eae6516cba30eb1a60ddb
Sean Hanson <shanson7@bloomberg.net>
2021-10-27 13:56:59 +0100

    Fix panic accessing empty slice

--- mdata/aggmetric.go
+++ mdata/aggmetric.go
@@ -189,7 +189,7 @@ func (a *AggMetric) GetAggregated(consolidator consolidation.Consolidator, aggSp
 			futurePoints := []schema.Point{}
 			if a.rob != nil {
 				futurePoints = a.rob.Get()
-				if result.Oldest > futurePoints[0].Ts {
+				if len(futurePoints) > 0 && result.Oldest > futurePoints[0].Ts {
 					result.Oldest = futurePoints[0].Ts
 				}
 			}
