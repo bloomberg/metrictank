7f6ac8da9ce3785eacc23c94ef3baa8c302f30dd
Dominik Jedruszczak <djedruszczak@bloomberg.net>
2021-11-08 11:14:52 -0500

    Use all flags in second example

--- cmd/mt-kafka-mdm-report-out-of-order/flags.go
+++ cmd/mt-kafka-mdm-report-out-of-order/flags.go
@@ -107,7 +107,7 @@ func (flags *Flags) Usage() {
 	fmt.Fprintln(os.Stderr)
 	fmt.Fprintln(os.Stderr, "EXAMPLES:")
 	fmt.Fprintln(os.Stderr, "  mt-kafka-mdm-report-out-of-order -group-by-name -config metrictank.ini -partition-from 0")
-	fmt.Fprintln(os.Stderr, "  mt-kafka-mdm-report-out-of-order -reorder-window 5 -group-by-tag namespace -config metrictank.ini -partition-from 0 -partition-to 3")
+	fmt.Fprintln(os.Stderr, "  mt-kafka-mdm-report-out-of-order -group-by-name -group-by-tag namespace -config metrictank.ini -partition-from 0 -partition-to 3 -reorder-window 5 -run-duration 5m")
 }
 
 func ParseFlags() Flags {
